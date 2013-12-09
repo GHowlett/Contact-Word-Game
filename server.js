fdae5632f732780e4c7d43f229aa3e70e2eb6211
GRIFFnDOOR <Griff.howl@gmail.com>
2013-12-09 15:41:24 -0800

    listened to incoming socket.io connections and broadcasted new players

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ var path = require("path"),
 server.use(express.static(__dirname + '/public'))
       .use(express.static(__dirname + '/bower_components'));
 
+var ioServer = http.createServer(server);
+var io = socketIO.listen(server);
+
+io.sockets.on("connection", function(client) {
+    client.on("named", function(name) {
+        client.broadcast.emit('joined', name);
+    });
+});
+
 var port = process.env.PORT || 3000;
-server.listen(port);
+ioServer.listen(port);
 console.log("Started server on port " + port);
