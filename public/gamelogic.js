3e00f5f249be5f7acc2d10db5e5907adf0c3ef16
GRIFFnDOOR <Griff.howl@gmail.com>
2013-12-15 01:35:46 -0800

    added missing curly braces

--- public/gamelogic.js
+++ public/gamelogic.js
@@ -177,7 +177,7 @@ function guessWord () {
 	}
 }
 
-function successConditions ()
+function successConditions () {
 
 	if (localPlayer !== clueGiver && localPlayer !== wordMaster) {
 		if(guess === clueGiver.secret) {
@@ -198,6 +198,7 @@ function successConditions ()
 			// advance to next round
 		}
 	}
+}
 
 /////////////////////////////////////////////////////////
 
