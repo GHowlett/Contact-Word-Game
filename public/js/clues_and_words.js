729e0a7d4bb86d730e6e30f52dc0dd4a08ecdf63
GRIFFnDOOR <Griff.howl@gmail.com>
2013-12-19 11:35:27 -0800

    refactored chooseWord()

--- public/js/clues_and_words.js
+++ public/js/clues_and_words.js
@@ -1,25 +1,24 @@
-function chooseWord (player) {
-	if (localPlayer ==! wordMaster) {
-		//capturing player word and running matchLetters check on revealed masterword letters
-		getInput("Type in a secret word " + wordMaster.word.slice(0, masterWordIndex +1) + "...", matchLetters)
+// allows local player to emit a word
+function chooseWord () {
+	if (localPlayer !== wordMaster) {
+		getInput("Type in a secret word that starts with" + 
+			wordMaster.word.slice(0, masterWordIndex +1) + "...", 
+			matchLetters)
 		
 		.done(function(word) {
-			player.word = word;
-			//once complete, choose clue 
+			localPlayer.word = word;
 			chooseClue();
 		})
-		//if word doesn't match master word letters
+		//if word letters don't match, retry
 		.fail(function(word) {
-			//change input box color to pink
 			this.css('background', '#FFDDDD')
-			getInput('First letters must match master word');
-			//after 3 seconds, allow player to choose word again
+			greyInput('First letters must match master word');
 			setTimeout(chooseWord, 3000);
 		})
 	}
 }
 
-//player words must match master word letters
+// player words must match master word letters
 function matchLetters (word) {
 	return word.slice(0, masterWordIndex +1) ===
 		wordMaster.word.slice(0, masterWordIndex +1);
