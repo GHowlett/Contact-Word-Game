58c47e2b37171688aa4be8d06781af3d284ed131
GRIFFnDOOR <Griff.howl@gmail.com>
2013-12-20 01:20:35 -0800

    yet another server error

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ function onGuess(guess) {
     if (guess.word === player.word) {
         if (guess.from !== masterName) {
             player.guesses.push(_.pick(guess, 'guess','from'));
-            if (players.length >= _.size(playerDB) /2) 
+            if (player.guesses.length >= _.size(playerDB) /2) 
                 startChallenge(player.name);
         }
         else endContact(player.name, false);
